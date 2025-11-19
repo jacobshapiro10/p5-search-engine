2b079dacab577995ea2ddff54274f1da7bf234bd
Jacob Shapiro <jacobshap518@gmail.com>
2025-11-18 21:11:09 -0500

    started

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,6 @@
-"""Map 0."""
+#!/usr/bin/env python3
+import sys
+
+for line in sys.stdin:
+    if "<!DOCTYPE html>" in line:
+        print(1)
--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,1 +1,11 @@
-"""Reduce 0."""
+#!/usr/bin/env python3
+import sys
+
+total = 0
+for line in sys.stdin:
+    line = line.strip()
+    if not line:
+        continue
+    total += int(line)
+
+print(total)

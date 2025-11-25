6f0d5cac91eae02931814060c39b3e28a45276a2
Levi Stein <steinlev@umich.edu>
2025-11-24 21:12:27 -0500

    Add search and database functionality: create searchdb script and configuration files

---
+++ search_server/search/config.py
@@ -0,0 +1,6 @@
+"""Configuration for the search server."""
+SEARCH_INDEX_SEGMENT_API_URLS = [
+    "http://localhost:9000/api/v1/hits/",
+    "http://localhost:9001/api/v1/hits/",
+    "http://localhost:9002/api/v1/hits/",
+]
